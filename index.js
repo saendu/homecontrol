87aca75aab3908494d1d33b4f27f27d2fd601fdb
Sandro Felder <sandro.felder@swisscom.com>
2020-07-22 11:11:21 +0200

    authentication middleware

--- index.js
+++ index.js
@@ -12,9 +12,33 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(basicAuth({
-  users: { 'admin': process.env.ADMIN_SECRET }
-}));
+// Authentication Middleware
+const authenticate = (req, res, next) => {
+  try {
+    // check for basic auth header
+      if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.status(401).json({ message: 'Missing Authorization Header' });
+    }
+
+    // verify auth credentials
+    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    const userMatches = basicAuth.safeCompare(username, process.env.ADMIN_USER);
+    const passwordMatches = basicAuth.safeCompare(password, process.env.ADMIN_SECRET);
+
+    if(userMatches & passwordMatches) {
+      next(); // only pass when successful
+    }
+    else {
+      return res.status(403).json({ message: 'Access denied: wrong user or password' });
+    }
+  }
+  catch(err) {
+    console.log(`Authentication error: ${err}`);
+    res.status(500).json({ message: `Internal error: ${err}` });
+  }
+}
 
 // Init middleware
 app.use(logger);
@@ -41,6 +65,6 @@ app.get('/', (req, res) =>
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API Routes
-app.use(`/${APP_URL_PREFIX}/plugs`, require('./routes/api/plugs'));
+app.use(`/${APP_URL_PREFIX}/plugs`, authenticate, require('./routes/api/plugs'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} with URL PREFIX: ${APP_URL_PREFIX}`));
