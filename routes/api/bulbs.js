72ceb1a67218194814e575a347fda3b6879499ee
Sandro Felder <sandro.felder@swisscom.com>
2020-09-19 15:16:19 +0200

    add on/off bulb

--- routes/api/bulbs.js
+++ routes/api/bulbs.js
@@ -4,7 +4,7 @@ const stringify = require('json-stringify-safe');
 const deviceConnector = require('../../middleware/deviceconnector');
 
 router.get('/', (req, res) => {
-    //res.json(plugs);
+    //res.json(bulbs);
     res.send(stringify(deviceConnector.lightbulbs)); // TODO weird circle
 });
 
@@ -24,6 +24,42 @@ router.put('/:blubId/toggle', async (req, res) => {
     deviceConnector.restoreConnection();
   }
 
+  res.json(safeBulbInfo);
+});
+
+// Turn ON 
+
+router.put('/:blubId/on', async (req, res) => {
+  const id = req.params['blubId'];
+  const bulb = deviceConnector.lightbulbs[id];
+  const {client, ...safeBulbInfo} = bulb; 
+  
+  try {
+    await bulb.lightList[0].turnOn();
+  }
+  catch(error) {
+    console.log(error);
+    // restoring connection because this is 9/10 times the problem
+    deviceConnector.restoreConnection();
+  }
+  res.json(safeBulbInfo);
+});
+
+// Turn OFF 
+
+router.put('/:blubId/off', async (req, res) => {
+  const id = req.params['blubId'];
+  const bulb = deviceConnector.lightbulbs[id];
+  const {client, ...safeBulbInfo} = bulb; 
+  
+  try {
+    await bulb.lightList[0].turnOff();
+  }
+  catch(error) {
+    console.log(error);
+    // restoring connection because this is 9/10 times the problem
+    deviceConnector.restoreConnection();
+  }
   res.json(safeBulbInfo);
 });
 
