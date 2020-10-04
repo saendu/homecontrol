a3d6d648cc669da15580ce5595d44c63b2a9d3e6
Sandro Felder <sandro.felder@swisscom.com>
2020-10-04 11:44:19 +0200

    brightness and color support

--- routes/api/bulbs.js
+++ routes/api/bulbs.js
@@ -63,4 +63,41 @@ router.put('/:blubId/off', async (req, res) => {
   res.json(safeBulbInfo);
 });
 
+// Brightness
+router.put('/:blubId/brightness/:number', async (req, res) => {
+  const id = req.params['blubId'];
+  const number = req.params['number'];
+  const bulb = deviceConnector.lightbulbs[id];
+  const {client, ...safeBulbInfo} = bulb; 
+  
+  try {
+    await bulb.lightList[0].setBrightness(number);
+  }
+  catch(error) {
+    console.log(error);
+    // restoring connection because this is 9/10 times the problem
+    deviceConnector.restoreConnection();
+  }
+  res.json(safeBulbInfo);
+});
+
+// Brightness
+router.put('/:blubId/color/:color', async (req, res) => {
+  const id = req.params['blubId'];
+  // colors: f5faf6, f1e0b5, efd275
+  const color = req.params['color'];
+  const bulb = deviceConnector.lightbulbs[id];
+  const {client, ...safeBulbInfo} = bulb; 
+  
+  try {
+    await bulb.lightList[0].setColor(color);
+  }
+  catch(error) {
+    console.log(error);
+    // restoring connection because this is 9/10 times the problem
+    deviceConnector.restoreConnection();
+  }
+  res.json(safeBulbInfo);
+});
+
 module.exports = router;
